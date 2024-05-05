40bc3d664b92cd90806c63f7b30671df9dcf3d35
indiasfernandes <daniel.indias@gmail.com>
2024-05-06 00:35:54 +0100

    Updated settings to debug = False

--- CoinHub/settings.py
+++ CoinHub/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-exs@09)4oz810!7$sk)c#)(9hv-evxmsmafw3iswdu&f*6t_yz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '85.242.116.211']
 
 
 # Application definition
