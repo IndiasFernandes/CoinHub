8231240d18592c65c7441d27f581458387c04186
indiasfernandes <daniel.indias@gmail.com>
2024-05-25 22:40:18 +0100

    Updated DB name

--- apps/exchanges/forms.py
+++ apps/exchanges/forms.py
@@ -1,9 +1,10 @@
 # forms.py
-
-from django import forms
 from datetime import datetime, timedelta
 from .exchange_data import EXCHANGES
 from .models import Exchange
+from django import forms
+from .models import Market
+
 
 
 class DownloadDataForm(forms.Form):
@@ -33,3 +34,18 @@ class ExchangeForm(forms.ModelForm):
     class Meta:
         model = Exchange
         fields = ['name', 'description', 'api_url', 'api_key', 'secret_key', 'id_char']
+
+
+# In forms.py
+from django import forms
+from .models import Market, Exchange
+
+class MarketForm(forms.ModelForm):
+    exchange = forms.ModelChoiceField(queryset=Exchange.objects.all(), to_field_name='id_char', required=True)
+
+    class Meta:
+        model = Market
+        fields = ['market_type', 'coins', 'exchange']
+        widgets = {
+            'coins': forms.CheckboxSelectMultiple,
+        }
