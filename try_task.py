75ba820b0798cb25c64330266052cdd0acf5ff2c
indiasfernandes <daniel.indias@gmail.com>
2024-05-28 03:22:35 +0100

    Paper Trade Version 1

--- try_task.py
+++ try_task.py
@@ -1,25 +1,28 @@
-# run_task.py
+# try_task.py
 import os
 import django
 from celery import Celery
 
 # Set up Django settings and initialize Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CoinHub.settings_production')
-print(f"DJANGO_SETTINGS_MODULE: {os.environ.get('DJANGO_SETTINGS_MODULE')}")
 django.setup()
 
 # Initialize Celery
 app = Celery('CoinHub')
+app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Import the task
 from apps.market.tasks import run_paper_trading_task
 
 def main(trade_id):
-    result = run_paper_trading_task.apply_async(args=[trade_id])
-    print(f'Task ID: {result.id}')
-    print('Task has been triggered. Check your Celery worker logs for execution details.')
+    print("Starting the task execution...")
+    try:
+        result = run_paper_trading_task.apply_async(args=[trade_id])
+        print(f'Task ID: {result.id}')
+        print('Task has been triggered. Check your Celery worker logs for execution details.')
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 if __name__ == '__main__':
     trade_id = 1  # Replace with the trade_id you want to test
     main(trade_id)
-# s
