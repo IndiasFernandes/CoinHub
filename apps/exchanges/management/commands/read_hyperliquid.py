ace5046341cf339ec26a3a1d2bd63dc88df63024
indiasfernandes <daniel.indias@gmail.com>
2024-05-29 14:48:08 +0100

    Updated Graph Links

--- apps/exchanges/management/commands/read_hyperliquid.py
+++ apps/exchanges/management/commands/read_hyperliquid.py
@@ -13,12 +13,6 @@ class Command(BaseCommand):
                 bot_account.update_exchange_info()
                 bot_account.print_info()
 
-                # Capture the account value here if needed
-                account_value = bot_account.get_account_value()
-                print(f"Account Value: {account_value}")
-
-                # Sleep for 10 minutes (600 seconds) before the next record
-                time.sleep(600)
             except Exception as e:
                 self.stderr.write(f"Error occurred: {e}")
                 # Optional: Log the error to a file or monitoring system
