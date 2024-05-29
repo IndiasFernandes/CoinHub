e69eafaf6382121fd5b01d19e0fbcc1508cec2b6
indiasfernandes <daniel.indias@gmail.com>
2024-05-29 20:49:01 +0100

    Updated Graph Links

--- apps/exchanges/management/commands/read_hyperliquid.py
+++ apps/exchanges/management/commands/read_hyperliquid.py
@@ -8,13 +8,12 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         bot_account = BotAccount()  # Initialize your bot account
 
-        while True:
-            try:
-                bot_account.update_exchange_info()
-                bot_account.print_info()
+        try:
+            bot_account.update_exchange_info()
+            bot_account.print_info()
 
-            except Exception as e:
-                self.stderr.write(f"Error occurred: {e}")
-                # Optional: Log the error to a file or monitoring system
-                time.sleep(60)  # Continue to wait and try again after 10 minutes
+        except Exception as e:
+            self.stderr.write(f"Error occurred: {e}")
+            # Optional: Log the error to a file or monitoring system
+            time.sleep(60)  # Continue to wait and try again after 10 minutes
 
