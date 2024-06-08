cec928be589ede9fb8d2b5b1c679062ff1ef8dd7
indiasfernandes <daniel.indias@gmail.com>
2024-06-08 14:32:01 +0100

    Added Celery Log

--- CoinHub/celery.py
+++ CoinHub/celery.py
@@ -2,7 +2,8 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 from celery.schedules import crontab
-from CoinHub import settings
+from django.conf import settings
+import logging
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CoinHub.settings')
 
@@ -10,12 +11,19 @@ app = Celery('CoinHub')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
+# Setup basic logging
+logger = logging.getLogger(__name__)
+handler = logging.FileHandler('celery.log')
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
 app.conf.beat_schedule = {
     'run_read_hyperliquid_command_every_minute': {
         'task': 'apps.exchanges.tasks.run_read_hyperliquid_command',
-        'schedule': 60.0,  # Execute every 60 seconds
+        'schedule': crontab(),  # Executes every minute
     },
 }
 app.conf.beat_scheduler = 'django_celery_beat.schedulers:DatabaseScheduler'
-
 app.conf.timezone = 'UTC'
--- apps/market/tasks.py
+++ apps/market/tasks.py
@@ -1,21 +1,24 @@
 from celery import shared_task
 from apps.market.models import PaperTrade
 from apps.market.utils.trading_functions import paper_trade_execute
+import logging
+
+logger = logging.getLogger(__name__)
 
 @shared_task(bind=True, max_retries=3, default_retry_delay=60)
 def run_paper_trading_task(self, trade_id):
-    print('TASK TEST - Running Paper Trading Task')
+    logger.info('Running Paper Trading Task')
     try:
         trade = PaperTrade.objects.get(id=trade_id)
-        print(f"TASK TEST - PaperTrade object retrieved: {trade}")
+        logger.info(f'PaperTrade object retrieved: {trade}')
 
         if trade.is_active:
-            print(f"TASK TEST - Executing paper trade for active trade: {trade_id}")
+            logger.info(f'Executing paper trade for active trade: {trade_id}')
             paper_trade_execute(trade_id)
         else:
-            print(f"TASK TEST - Trade {trade_id} is not active.")
+            logger.info(f'Trade {trade_id} is not active.')
     except PaperTrade.DoesNotExist:
-        print(f"TASK TEST ERROR - No PaperTrade found for ID: {trade_id}")
+        logger.error(f'No PaperTrade found for ID: {trade_id}')
     except Exception as e:
-        print(f"TASK TEST ERROR - An error occurred: {str(e)}")
-        self.retry(exc=e, countdown=60)
+        logger.error(f'An error occurred: {str(e)}')
+        self.retry(exc=e)
